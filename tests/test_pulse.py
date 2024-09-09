830e9629108dec8f8df0baa7fbe64069540ca49e
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2024-09-09 23:46:38 +0100

    Parametrized test

--- tests/test_pulse.py
+++ tests/test_pulse.py
@@ -2,19 +2,29 @@ import numpy as np
 import pytest
 from inline_snapshot import snapshot
 
-from promdens.promdens import LaserPulse
+from promdens.promdens import ENVELOPE_TYPES, LaserPulse
 
 def test_invalid_envelope_type(make_pulse):
     with pytest.raises(ValueError):
         make_pulse(envelope_type='invalid')
 
-#ENVELOPE_TYPES = ['gauss', 'lorentz', 'sech', 'sin', 'sin2']
-def test_field_cos(make_pulse):
-    pulse = make_pulse(lchirp=0.01)
+@pytest.mark.parametrize("envelope", ENVELOPE_TYPES)
+def test_field_cos(make_pulse, envelope):
+    pulse = make_pulse(lchirp=0.01, envelope_type=envelope)
     t = np.array([0.0, 1.0, 10.0])
     cos = pulse.field_cos(t)
 
+    s = snapshot(
+        {
+            "gauss": {0: 1.0, 1: 0.9939560979566968, 2: -0.4161468365471424},
+            "lorentz": {0: 1.0, 1: 0.9939560979566968, 2: -0.4161468365471424},
+            "sech": {0: 1.0, 1: 0.9939560979566968, 2: -0.4161468365471424},
+            "sin": {0: 1.0, 1: 0.9939560979566968, 2: -0.4161468365471424},
+            "sin2": {0: 1.0, 1: 0.9939560979566968, 2: -0.4161468365471424},
+        }
+    )
+
     assert len(cos) == len(t)
-    assert cos[0] == snapshot(1.0)
-    assert cos[1] == snapshot(0.9939560979566968)
-    assert cos[2] == snapshot(-0.4161468365471424)
+    assert cos[0] == s[envelope][0]
+    assert cos[1] == s[envelope][1]
+    assert cos[2] == s[envelope][2]
