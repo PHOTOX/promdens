13cbe9e562c97520c516944ab5089b017c9e9469
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2024-09-09 23:48:36 +0100

    Less cool test

--- tests/test_pulse.py
+++ tests/test_pulse.py
@@ -8,23 +8,15 @@ def test_invalid_envelope_type(make_pulse):
     with pytest.raises(ValueError):
         make_pulse(envelope_type='invalid')
 
-@pytest.mark.parametrize("envelope", ENVELOPE_TYPES)
-def test_field_cos(make_pulse, envelope):
-    pulse = make_pulse(lchirp=0.01, envelope_type=envelope)
+#@pytest.mark.parametrize("envelope", ENVELOPE_TYPES)
+def test_field_cos(make_pulse):
+    pulse = make_pulse(lchirp=0.01, omega=0.1)
     t = np.array([0.0, 1.0, 10.0])
     cos = pulse.field_cos(t)
 
-    s = snapshot(
-        {
-            "gauss": {0: 1.0, 1: 0.9939560979566968, 2: -0.4161468365471424},
-            "lorentz": {0: 1.0, 1: 0.9939560979566968, 2: -0.4161468365471424},
-            "sech": {0: 1.0, 1: 0.9939560979566968, 2: -0.4161468365471424},
-            "sin": {0: 1.0, 1: 0.9939560979566968, 2: -0.4161468365471424},
-            "sin2": {0: 1.0, 1: 0.9939560979566968, 2: -0.4161468365471424},
-        }
-    )
+    s = snapshot({0: 1.0, 1: 0.9939560979566968, 2: -0.4161468365471424})
 
     assert len(cos) == len(t)
-    assert cos[0] == s[envelope][0]
-    assert cos[1] == s[envelope][1]
-    assert cos[2] == s[envelope][2]
+    assert cos[0] == s[0]
+    assert cos[1] == s[1]
+    assert cos[2] == s[2]
