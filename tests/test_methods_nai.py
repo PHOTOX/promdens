f10f44264c893ab43d59113c99d463785b07cc0b
Jiri Janos <jiri.janos98@gmail.com>
2024-09-12 14:10:30 +0200

    Adding two tests with NaI, comparing to exact quantum dynamics as reference.

---
+++ tests/test_methods_nai.py
@@ -0,0 +1,58 @@
+"""This test aims to compare to previous PDA and PDAW results that were published and compared to exact QD.
+We use NaI as an example with two different laser pulses. If this test fails, the results produced by the code are unreliable!"""
+
+import numpy as np
+
+from promdens.promdens import InitialConditions, LaserPulse
+
+
+def test_pdaw_nai(make_pulse):
+    ics = InitialConditions(nsamples=1000, nstates=1, input_type='file')
+
+    # todo: this file path works only if 'pytest' is launched in the main folder (the same for all later)
+    ics.read_input_data(fname='tests/NaI_reference/test_data_nai.dat', energy_unit='a.u.', tdm_unit='a.u.')
+
+    fstoau = 41.341374575751
+    pulse = LaserPulse(omega=0.14294844, fwhm=100*fstoau, envelope_type='gauss', lchirp=2e-6, t0=0)
+
+    ics.calc_field(pulse=pulse)
+
+    ics.windowing()
+
+    # the following command was used to generate the reference
+    # todo: use different pulse
+    # the reference was compared to exact QD
+    reference = np.genfromtxt('tests/NaI_reference/test_pdaw_reference.dat').T[1]
+    weights = ics.weights[0]
+
+    assert len(weights) == len(weights)
+    # comparing all weights
+    for i in range(len(weights)):
+        assert weights[i] == reference[i]
+
+
+def test_pda_nai(make_pulse):
+    ics = InitialConditions(nsamples=1000, nstates=1, input_type='file')
+
+    ics.read_input_data(fname='tests/NaI_reference/test_data_nai.dat', energy_unit='a.u.', tdm_unit='a.u.')
+
+    fstoau = 41.341374575751
+    pulse = LaserPulse(omega=0.14294844, fwhm=100*fstoau, envelope_type='gauss', lchirp=2e-6, t0=0)
+
+    ics.calc_field(pulse=pulse)
+
+    ics.sample_initial_conditions(nsamples_ic=500, neg_handling='error', preselect=True, seed=123456789)
+
+    # the following command was used to generate the reference
+    # promdens -np 500 -w 0.14294844 -lch 2e-6 -f 100 -ps --random_seed 123456789 -n 1000 test_data_nai.dat
+    # the reference was compared to exact QD
+    reference = np.genfromtxt('tests/NaI_reference/test_pda_reference.dat').T
+    pda = ics.ics
+
+    assert len(pda) == len(reference)
+    # testing all generated ICs, same indexes and excitation times are required
+    for i in range(len(pda)):
+        # comparing indexes
+        assert pda[0, i] == reference[0, i]
+        # comparing excitation times
+        assert pda[1, i] == reference[1, i]
