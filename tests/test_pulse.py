5cc4db23ac25cfc511bc667250015556f6763107
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2024-09-15 17:17:48 +0100

    Test envelope more

--- tests/test_pulse.py
+++ tests/test_pulse.py
@@ -72,27 +72,145 @@ def test_field_cos_with_chirp(make_pulse):
 
 @pytest.mark.parametrize("envelope_type", ENVELOPE_TYPES)
 def test_field_envelope(make_pulse, envelope_type):
-    fwhm = 10.
-    t0 = -1
+    fwhm = 15.
+    t0 = 0
     pulse = make_pulse(fwhm=fwhm, t0=t0, envelope_type=envelope_type)
 
-    # TODO: We need more points here (e.g. beyond extent)
-    # t = np.array([-50, -10, -5.0, -1.0, 4.0, 10, 50])
-    # Sanity check: for t=t0 the value should be always 1.0
-    t = np.array([-5.0, -1.0, 4.0])
+    t = np.array([-64, -16, -4.0, 0.0, 4.0, 16, 64])
 
     envelope = pulse.calc_field_envelope(t)
 
     s = snapshot(
         {
-            "gauss": {0: 0.8010698775896221, 1: 1.0, 2: 0.7071067811865476},
-            "lorentz": {0: 0.7904534221560047, 1: 1.0, 2: 0.7071067811865475},
-            "sech": {0: 0.7942475773796802, 1: 1.0, 2: 0.7071067811865476},
-            "sin": {0: 0.8090169943749475, 1: 1.0, 2: 0.7071067811865476},
-            "sin2": {0: 0.8049055147147319, 1: 1.0, 2: 0.7071067811865475},
+            "gauss": {
+                0: 1.0960549698939707e-11,
+                1: 0.20653303211845794,
+                2: 0.9061223550383886,
+                3: 1.0,
+                4: 0.9061223550383886,
+                5: 0.20653303211845794,
+                6: 1.0960549698939707e-11,
+            },
+            "lorentz": {
+                0: 0.03209025202400356,
+                1: 0.346604598571491,
+                2: 0.8945978172388421,
+                3: 1.0,
+                4: 0.8945978172388421,
+                5: 0.346604598571491,
+                6: 0.03209025202400356,
+            },
+            "sech": {
+                0: 0.001083121940752962,
+                1: 0.2981611144195047,
+                2: 0.8988518704633442,
+                3: 1.0,
+                4: 0.8988518704633442,
+                5: 0.2981611144195047,
+                6: 0.001083121940752962,
+            },
+            "sin": {
+                0: 0.0,
+                1: 0.0,
+                2: 0.9135454576426009,
+                3: 1.0,
+                4: 0.913545457642601,
+                5: 0.0,
+                6: 0.0,
+            },
+            "sin2": {
+                0: 0.0,
+                1: 0.11811460943703252,
+                2: 0.90982893698841,
+                3: 1.0,
+                4: 0.90982893698841,
+                5: 0.11811460943703297,
+                6: 0.0,
+            },
         }
     )
 
     assert len(envelope) == len(t)
+
+    # Maximum at t0 is always 1.0
+    assert envelope[3] == 1.0
     for i, value in enumerate(envelope):
         assert value == s[envelope_type][i]
+
+    assert envelope[0] == envelope[-1]
+    if envelope_type not in  ("sin", "sin2"):
+        assert envelope[1] == envelope[-2]
+        assert envelope[2] == envelope[-3]
+
+@pytest.mark.parametrize("envelope_type", ENVELOPE_TYPES)
+def test_field_envelope_shifted(make_pulse, envelope_type):
+    fwhm = 20.
+    t0 = 2.0
+    pulse = make_pulse(fwhm=fwhm, t0=t0, envelope_type=envelope_type)
+
+    t = np.array([-60, -12, 0.0, 2.0, 4.0, 16, 64])
+
+    envelope = pulse.calc_field_envelope(t)
+
+    s = snapshot(
+        {
+            "gauss": {
+                0: 1.6375836113696163e-06,
+                1: 0.5069797398950145,
+                2: 0.9862327044933592,
+                3: 1.0,
+                4: 0.9862327044933592,
+                5: 0.5069797398950145,
+                6: 1.6375836113696163e-06,
+            },
+            "lorentz": {
+                0: 0.05909337989549738,
+                1: 0.5519194543083392,
+                2: 0.9837014998966422,
+                3: 1.0,
+                4: 0.9837014998966422,
+                5: 0.5519194543083392,
+                6: 0.05909337989549738,
+            },
+            "sech": {
+                0: 0.008468626120468338,
+                1: 0.5367937766740093,
+                2: 0.9846622513085862,
+                3: 1.0,
+                4: 0.9846622513085862,
+                5: 0.5367937766740093,
+                6: 0.008468626120468338,
+            },
+            "sin": {
+                0: 0.0,
+                1: 0.45399049973954675,
+                2: 0.9876883405951378,
+                3: 1.0,
+                4: 0.9876883405951378,
+                5: 0.45399049973954686,
+                6: 0.0,
+            },
+            "sin2": {
+                0: 0.0,
+                1: 0.4847980881794031,
+                2: 0.9869760345887517,
+                3: 1.0,
+                4: 0.9869760345887517,
+                5: 0.4847980881794033,
+                6: 0.0,
+            },
+        }
+    )
+
+    assert len(envelope) == len(t)
+    # Maximum at t0 is always 1.0
+    assert envelope[3] == 1.0
+    for i, value in enumerate(envelope):
+        assert value == s[envelope_type][i]
+
+    # envelope an even function so is symmetrical around t0
+    assert envelope[0] == envelope[-1]
+    # There are minor numerical differences with sin and sin2 envelopes
+    if envelope_type not in  ("sin", "sin2"):
+        assert envelope[1] == envelope[-2]
+        assert envelope[2] == envelope[-3]
