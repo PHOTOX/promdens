5cc41ccd04a84e0fe822382645d134d69f2ca105
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2024-09-17 23:10:17 +0100

    Add more asserts about temporal extent

--- tests/test_pulse.py
+++ tests/test_pulse.py
@@ -152,6 +152,9 @@ def test_field_envelope(make_pulse, envelope_type):
 
     assert pulse.tmin == s_tmin[envelope_type]
     assert pulse.tmax == s_tmax[envelope_type]
+    assert pulse.tmin < t0
+    assert pulse.tmax > t0
+    assert pulse.tmin == -pulse.tmax
 
     # Maximum at t0 is always 1.0
     assert envelope[3] == 1.0
@@ -250,6 +253,9 @@ def test_field_envelope_shifted(make_pulse, envelope_type):
 
     assert pulse.tmin == s_tmin[envelope_type]
     assert pulse.tmax == s_tmax[envelope_type]
+    assert pulse.tmin < t0
+    assert pulse.tmax > t0
+    assert pulse.tmin == -pulse.tmax
 
     # Maximum at t0 is always 1.0
     assert envelope[3] == 1.0
