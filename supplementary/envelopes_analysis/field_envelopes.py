06c797b0f6cdd726d927960584ba9b2e251fcc91
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2024-09-19 16:18:18 +0100

    Ruff format

--- supplementary/envelopes_analysis/field_envelopes.py
+++ supplementary/envelopes_analysis/field_envelopes.py
@@ -12,7 +12,7 @@ fwhm = 10  # in fs
 omega = 1  # in a.u.
 lchirp = 0  # in a.u.
 t0 = 0  # in fs
-envelope_types = ['sin', 'sin2', 'gauss', 'sech', 'lorentz']
+envelope_types = ["sin", "sin2", "gauss", "sech", "lorentz"]
 
 # conversion of units
 evtoau = 0.036749405469679
@@ -37,55 +37,55 @@ for i in range(len(envelope_types)):
     field.calc_field(pulse=pulse)
     field.field_ft_omega -= omega
 
-    axs[0, 0].plot(field.field_t/field.fstoau, field.field_envelope, color=colors[i], label=f"'{envelope_type}'")
-    axs[0, 1].plot(field.field_t/field.fstoau, field.field_envelope**2, color=colors[i], label=f"'{envelope_type}'")
-    axs[1, 0].plot(field.field_ft_omega/field.evtoau, field.field_ft, color=colors[i], label=f"'{envelope_type}'")
-    axs[1, 1].plot(field.field_ft_omega/field.evtoau, field.field_ft**2, color=colors[i], label=f"'{envelope_type}'")
+    axs[0, 0].plot(field.field_t / field.fstoau, field.field_envelope, color=colors[i], label=f"'{envelope_type}'")
+    axs[0, 1].plot(field.field_t / field.fstoau, field.field_envelope**2, color=colors[i], label=f"'{envelope_type}'")
+    axs[1, 0].plot(field.field_ft_omega / field.evtoau, field.field_ft, color=colors[i], label=f"'{envelope_type}'")
+    axs[1, 1].plot(field.field_ft_omega / field.evtoau, field.field_ft**2, color=colors[i], label=f"'{envelope_type}'")
 
-axs[0, 0].axhline(0.5, color='black', linestyle='--', lw=0.5)
-axs[0, 0].set_xlim(-3*fwhm/fstoau, 3*fwhm/fstoau)
+axs[0, 0].axhline(0.5, color="black", linestyle="--", lw=0.5)
+axs[0, 0].set_xlim(-3 * fwhm / fstoau, 3 * fwhm / fstoau)
 axs[0, 0].set_ylim(0, 1.2)
 axs[0, 0].set_xlabel(r"$t$ (fs)")
 axs[0, 0].set_ylabel(r"$\varepsilon(t)$")
 axs[0, 0].set_title(r"Pulse envelope")
-axs[0, 0].legend(frameon=False, labelspacing=0.1, loc='upper right')
+axs[0, 0].legend(frameon=False, labelspacing=0.1, loc="upper right")
 
-fwhm2 = fwhm/2/fstoau
-axs[0, 1].plot([-fwhm2, -fwhm2, fwhm2, fwhm2], [0, 0.5, 0.5, 0], color='black', linestyle='--')
-axs[0, 1].text(0, 0.2, r"$\tau_\mathregular{FWHM}$", horizontalalignment='center', color='black')
-axs[0, 1].set_xlim(-3*fwhm/fstoau, 3*fwhm/fstoau)
+fwhm2 = fwhm / 2 / fstoau
+axs[0, 1].plot([-fwhm2, -fwhm2, fwhm2, fwhm2], [0, 0.5, 0.5, 0], color="black", linestyle="--")
+axs[0, 1].text(0, 0.2, r"$\tau_\mathregular{FWHM}$", horizontalalignment="center", color="black")
+axs[0, 1].set_xlim(-3 * fwhm / fstoau, 3 * fwhm / fstoau)
 axs[0, 1].set_ylim(0, 1.2)
 axs[0, 1].set_xlabel(r"$t$ (fs)")
 axs[0, 1].set_ylabel(r"$I(t)$")
 axs[0, 1].set_title(r"Pulse intensity")
-axs[0, 1].legend(frameon=False, labelspacing=0.1, loc='upper right')
+axs[0, 1].legend(frameon=False, labelspacing=0.1, loc="upper right")
 
-axs[1, 0].set_xlim(-2*np.pi/fwhm*fstoau, 2*np.pi/fwhm*fstoau)
+axs[1, 0].set_xlim(-2 * np.pi / fwhm * fstoau, 2 * np.pi / fwhm * fstoau)
 axs[1, 0].set_ylim(0, 1.2)
 axs[1, 0].set_xlabel(r"$E$ (eV)")
 axs[1, 0].set_ylabel(r"$|\varepsilon(\omega)|$")
 axs[1, 0].set_title(r"Envelope spectrum")
-axs[1, 0].legend(frameon=False, labelspacing=0.1, loc='upper right')
+axs[1, 0].legend(frameon=False, labelspacing=0.1, loc="upper right")
 
-axs[1, 1].set_xlim(-2*np.pi/fwhm*fstoau, 2*np.pi/fwhm*fstoau)
+axs[1, 1].set_xlim(-2 * np.pi / fwhm * fstoau, 2 * np.pi / fwhm * fstoau)
 axs[1, 1].set_ylim(0, 1.2)
 axs[1, 1].set_xlabel(r"$E$ (eV)")
 axs[1, 1].set_ylabel(r"$S(\omega)$")
 axs[1, 1].set_title(r"Spectral intensity")
-axs[1, 1].legend(frameon=False, labelspacing=0.1, loc='upper right')
+axs[1, 1].legend(frameon=False, labelspacing=0.1, loc="upper right")
 
 plt.tight_layout()
-plt.savefig('implemented_envelopes', dpi=300)
+plt.savefig("implemented_envelopes", dpi=300)
 plt.show(block=False)
 
 # plotting Wigner transformations of the field
 print("\nWigner transfroms of the pulses")
-fig, axs = plt.subplots(2, len(envelope_types), figsize=(2.5*len(envelope_types), 5), sharex=True, sharey=True)
+fig, axs = plt.subplots(2, len(envelope_types), figsize=(2.5 * len(envelope_types), 5), sharex=True, sharey=True)
 
 # create a 2D map
 grid = 250
-e = np.linspace(-1.5*np.pi/fwhm*fstoau, 1.5*np.pi/fwhm*fstoau, grid)
-t = np.linspace(-2.1*fwhm/fstoau, 2.1*fwhm/fstoau, grid)
+e = np.linspace(-1.5 * np.pi / fwhm * fstoau, 1.5 * np.pi / fwhm * fstoau, grid)
+t = np.linspace(-2.1 * fwhm / fstoau, 2.1 * fwhm / fstoau, grid)
 e2d, t2d = np.meshgrid(e, t)
 
 for i in range(len(envelope_types)):
@@ -97,28 +97,30 @@ for i in range(len(envelope_types)):
 
     for j in range(len(t)):
         for k in range(len(e)):
-            pulse_wigner[j, k] = field.pulse_wigner(tprime=t2d[j, k]*fstoau, de=e2d[j, k]*evtoau + omega)
+            pulse_wigner[j, k] = field.pulse_wigner(tprime=t2d[j, k] * fstoau, de=e2d[j, k] * evtoau + omega)
 
     pulse_wigner /= np.max(np.abs(pulse_wigner))
-    pc = axs[0, i].pcolormesh(e2d, t2d, pulse_wigner, cmap='RdBu', vmin=-1, vmax=1)
-    if i == len(envelope_types) - 1: fig.colorbar(pc, ax=axs[0, i], shrink=0.92, fraction=0.05)
+    pc = axs[0, i].pcolormesh(e2d, t2d, pulse_wigner, cmap="RdBu", vmin=-1, vmax=1)
+    if i == len(envelope_types) - 1:
+        fig.colorbar(pc, ax=axs[0, i], shrink=0.92, fraction=0.05)
 
-    pc = axs[1, i].pcolormesh(e2d, t2d, pulse_wigner, cmap='Blues', norm='log', vmin=1e-5, vmax=1)
-    if i == len(envelope_types) - 1: fig.colorbar(pc, ax=axs[1, i], shrink=0.92, fraction=0.05)
-    pc = axs[1, i].pcolormesh(e2d, t2d, -pulse_wigner, cmap='Reds', norm='log', vmin=1e-5, vmax=1)
+    pc = axs[1, i].pcolormesh(e2d, t2d, pulse_wigner, cmap="Blues", norm="log", vmin=1e-5, vmax=1)
+    if i == len(envelope_types) - 1:
+        fig.colorbar(pc, ax=axs[1, i], shrink=0.92, fraction=0.05)
+    pc = axs[1, i].pcolormesh(e2d, t2d, -pulse_wigner, cmap="Reds", norm="log", vmin=1e-5, vmax=1)
 
     axs[1, i].set_xlabel(r"$\Delta E$ (eV)")
     axs[0, i].set_title(f"'{envelope_type}'")
-    axs[0, i].tick_params('both', direction='in', which='both', top=True, right=True)
-    axs[1, i].tick_params('both', direction='in', which='both', top=True, right=True)
+    axs[0, i].tick_params("both", direction="in", which="both", top=True, right=True)
+    axs[1, i].tick_params("both", direction="in", which="both", top=True, right=True)
 
 axs[0, 0].set_ylabel(r"$t$ (fs)")
 axs[1, 0].set_ylabel(r"$t$ (fs)")
 
-axs[0, 0].text(x=np.min(e) + 0.03, y=np.max(t) - 0.15*fwhm/fstoau, s=r"$\mathcal{W}_E(t,\Delta E)$", va="top", ha="left")
-axs[1, 0].text(x=np.min(e) + 0.03, y=np.max(t) - 0.15*fwhm/fstoau, s=r"$\ln[\mathcal{W}_E(t,\Delta E)]$", va="top", ha="left")
+axs[0, 0].text(x=np.min(e) + 0.03, y=np.max(t) - 0.15 * fwhm / fstoau, s=r"$\mathcal{W}_E(t,\Delta E)$", va="top", ha="left")
+axs[1, 0].text(x=np.min(e) + 0.03, y=np.max(t) - 0.15 * fwhm / fstoau, s=r"$\ln[\mathcal{W}_E(t,\Delta E)]$", va="top", ha="left")
 
 plt.tight_layout()
 fig.subplots_adjust(wspace=0, hspace=0)
-plt.savefig('envelope_wigner_transform', dpi=300)
+plt.savefig("envelope_wigner_transform", dpi=300)
 plt.show()
--- tests/test_methods_nai.py
+++ tests/test_methods_nai.py
@@ -13,21 +13,21 @@ path = Path(__file__).parent.absolute()
 
 
 def test_pdaw_nai(tmp_path):
-    ics = InitialConditions(nsamples=10, nstates=1, input_type='file')
+    ics = InitialConditions(nsamples=10, nstates=1, input_type="file")
 
-    ics.read_input_data(fname=path/'NaI_reference/test_data_nai.dat', energy_unit='a.u.', tdm_unit='a.u.')
+    ics.read_input_data(fname=path / "NaI_reference/test_data_nai.dat", energy_unit="a.u.", tdm_unit="a.u.")
 
     fstoau = 41.341374575751
-    pulse = LaserPulse(omega=0.13520905, fwhm=20*fstoau, envelope_type='gauss', lchirp=0, t0=0)
+    pulse = LaserPulse(omega=0.13520905, fwhm=20 * fstoau, envelope_type="gauss", lchirp=0, t0=0)
 
     ics.calc_field(pulse=pulse)
 
-    ics.windowing(output_fname=tmp_path/'pdaw.dat')
+    ics.windowing(output_fname=tmp_path / "pdaw.dat")
 
     # the following command was used to generate the reference
     # promdens -m pdaw -n 10 -w 0.13520905 -f 20 test_data_nai.dat
     # the reference was compared to exact QD
-    reference = np.genfromtxt(path/'NaI_reference/test_pdaw_reference.dat').T[1]
+    reference = np.genfromtxt(path / "NaI_reference/test_pdaw_reference.dat").T[1]
     weights = ics.weights[0]
 
     assert len(weights) == len(weights)
@@ -35,25 +35,29 @@ def test_pdaw_nai(tmp_path):
     for i in range(len(weights)):
         # the tolerance is the larger of the absolute and relative thresholds
         # equivalent to (weights[i] - reference[i]) < max([1e-18, reference[i]*1e-15])
-        assert weights[i] == pytest.approx(reference[i], abs=1e-18, rel=1e-15), f"pdaw weight[{i}] does not match: delta={weights[i] - reference[i]}"
+        assert weights[i] == pytest.approx(
+            reference[i], abs=1e-18, rel=1e-15
+        ), f"pdaw weight[{i}] does not match: delta={weights[i] - reference[i]}"
 
 
 def test_pda_nai(tmp_path):
-    ics = InitialConditions(nsamples=1000, nstates=1, input_type='file')
+    ics = InitialConditions(nsamples=1000, nstates=1, input_type="file")
 
-    ics.read_input_data(fname=path/'NaI_reference/test_data_nai.dat', energy_unit='a.u.', tdm_unit='a.u.')
+    ics.read_input_data(fname=path / "NaI_reference/test_data_nai.dat", energy_unit="a.u.", tdm_unit="a.u.")
 
     fstoau = 41.341374575751
-    pulse = LaserPulse(omega=0.14294844, fwhm=100*fstoau, envelope_type='gauss', lchirp=2e-6, t0=0)
+    pulse = LaserPulse(omega=0.14294844, fwhm=100 * fstoau, envelope_type="gauss", lchirp=2e-6, t0=0)
 
     ics.calc_field(pulse=pulse)
 
-    ics.sample_initial_conditions(nsamples_ic=10, neg_handling='error', preselect=True, seed=123456789, output_fname=tmp_path/'pda.dat')
+    ics.sample_initial_conditions(
+        nsamples_ic=10, neg_handling="error", preselect=True, seed=123456789, output_fname=tmp_path / "pda.dat"
+    )
 
     # the following command was used to generate the reference
     # promdens -np 10 -w 0.14294844 -lch 2e-6 -f 100 -ps --random_seed 123456789 -n 10 test_data_nai.dat
     # the reference was compared to exact QD
-    reference = np.genfromtxt(path/'NaI_reference/test_pda_reference.dat').T
+    reference = np.genfromtxt(path / "NaI_reference/test_pda_reference.dat").T
     pda = ics.ics
 
     assert len(pda) == len(reference)
@@ -64,4 +68,6 @@ def test_pda_nai(tmp_path):
         # comparing excitation times, the thresholds were based on numerical differences created by switching between numpy 1.26 and 2.1
         # the tolerance is the larger of the absolute and relative thresholds
         # equivalent to (pda[1, i] - reference[1, i]) < max([1e-18, reference[1, i]*1e-13])
-        assert pda[1, i] == pytest.approx(reference[1, i], abs=1e-18, rel=1e-13), f"pda excitation time for index {i} does not match: delta = {pda[1, i] - reference[1, i]}"
+        assert pda[1, i] == pytest.approx(
+            reference[1, i], abs=1e-18, rel=1e-13
+        ), f"pda excitation time for index {i} does not match: delta = {pda[1, i] - reference[1, i]}"
